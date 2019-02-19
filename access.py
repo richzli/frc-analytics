8b40189dd48d499acf6f4edac21d13f85954a44a
Richard Li <diamondspell13@gmail.com>
2019-02-19 09:57:59 -0600

    created starter code

---
+++ access.py
@@ -0,0 +1,14 @@
+import base64
+from urllib.request import Request, urlopen
+import json
+
+tokenfile = open("token.secret", "r")
+token = str(base64.b64encode(tokenfile.readline().encode()))[2:-1]
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": "Basic " + token
+}
+
+request = Request(URL HERE)
+response = urlopen(request).read()
