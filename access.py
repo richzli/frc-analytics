209b9e7c24a7f634470a6b45d67aaccaca8ca171
Richard Li <diamondspell13@gmail.com>
2019-02-20 15:20:32 -0600

    one step closer to a workable csv

--- access.py
+++ access.py
@@ -1,6 +1,7 @@
 import base64
 from urllib.request import Request, urlopen
 import json
+import csv #https://docs.python.org/3/library/csv.html
 
 tokenfile = open("token.secret", "r")
 token = str(base64.b64encode(tokenfile.readline().encode()))[2:-1]
@@ -10,7 +11,22 @@ headers = {
     "Authorization": "Basic " + token
 }
 
-request = Request("https://frc-api.firstinspires.org/v2.0/2017/matches/CMPMO", headers=headers)
+year = input("Season Year: ")
+eventcode = input("Event Code: ")
+
+request = Request("https://frc-api.firstinspires.org/v2.0/"+year+"/matches/"+eventcode,
+                  headers=headers) #This is the prod server! Change to staging once fixed.
 response = urlopen(request).read()
 
-data = json.loads(response)["Matches"]
+matchdata = json.loads(response)["Matches"]
+
+datafile = open("data/"+year+eventcode+".csv", "w+")
+csvwriter = csv.writer(datafile)
+
+header = matchdata[0].keys()
+csvwriter.writerow(header)
+
+for match in matchdata:
+    csvwriter.writerow(match.values())
+
+datafile.close()
